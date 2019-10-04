cce3f6937cf69ae326cca75813281478e59f6da4
ginaperez <gmperez92@gmail.com>
2019-10-04 12:22:00 -0700

    committing for testnig

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: []
+    };
+  }
+  
+  componentDidMount() {
+    axios.get("/api/students").then(response => {
+      console.log(response.data);
+    });
+  }
+  
+  render() {
+    const mappedStudents = this.state.students.map(student => {
+      return (
+      <div key={student.id}>
+        <div>{student.name}</div>
+        <div>{student.id}</div>
+      </div>
+    );
+  });
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Students</h1>
+    {mappedStudents}
     </div>
-  );
+    );
+}
 }
 
 export default App;
