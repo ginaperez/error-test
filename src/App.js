07e7141766d8c957aa4fdec5375d8ef0a4674150
ginaperez <gmperez92@gmail.com>
2019-10-04 17:45:26 -0700

    gina perez changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   }
   
   componentDidMount() {
-    axios.get("/api/students").then(response => {
+    axios.get("http://localhost:4000/api/students").then(response => {
       console.log(response.data);
     });
   }
